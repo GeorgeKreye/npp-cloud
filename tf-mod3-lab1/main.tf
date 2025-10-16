e842a9dc9cba040c19f7feba2438482a43fbe3a7
George Kreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-10-16 16:25:59 -0600

    formatting

--- tf-mod3-lab1/main.tf
+++ tf-mod3-lab1/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.51.0"
     }
   }
@@ -9,12 +9,12 @@ terraform {
 
 // VPCs
 resource "google_compute_network" "tf-mod3-lab1-vpc1" {
-  name = "tf-mod3-lab1-vpc1"
+  name                    = "tf-mod3-lab1-vpc1"
   auto_create_subnetworks = "false"
 }
 
 resource "google_compute_network" "tf-mod3-lab1-vpc2" {
-  name = "tf-mod3-lab1-vpc2"
+  name                    = "tf-mod3-lab1-vpc2"
   auto_create_subnetworks = "false"
 }
 
@@ -38,14 +38,14 @@ resource "google_compute_subnetwork" "tf-mod3-lab1-sub2" {
 // TODO: add vxlan ports
 resource "google_compute_firewall" "tf-mod3-lab1-fwrule1" {
   project = "kreye-lab1project-cunetworking"
-  name        = "tf-mod3-lab1-fwrule1"
-  network     = "tf-mod3-lab1-vpc1"
+  name    = "tf-mod3-lab1-fwrule1"
+  network = "tf-mod3-lab1-vpc1"
   // need the network created before the firewall rule
   depends_on = [google_compute_network.tf-mod3-lab1-vpc1]
 
   allow {
-    protocol  = "tcp"
-    ports     = ["22", "1234", "50000"]
+    protocol = "tcp"
+    ports    = ["22", "1234", "50000"]
   }
   allow {
     protocol = "icmp"
@@ -55,14 +55,14 @@ resource "google_compute_firewall" "tf-mod3-lab1-fwrule1" {
 
 resource "google_compute_firewall" "tf-mod3-lab1-fwrule2" {
   project = "kreye-lab1project-cunetworking"
-  name        = "tf-mod3-lab1-fwrule2"
-  network     = "tf-mod3-lab1-vpc2"
+  name    = "tf-mod3-lab1-fwrule2"
+  network = "tf-mod3-lab1-vpc2"
   // need the network created before the firewall rule
   depends_on = [google_compute_network.tf-mod3-lab1-vpc2]
 
   allow {
-    protocol  = "tcp"
-    ports     = ["22", "1234", "50000"]
+    protocol = "tcp"
+    ports    = ["22", "1234", "50000"]
   }
   allow {
     protocol = "icmp"
@@ -72,34 +72,34 @@ resource "google_compute_firewall" "tf-mod3-lab1-fwrule2" {
 
 // Router
 resource "google_compute_router" "tf-mod3-lab1-router" {
-    name = "tf-mod3-lab1-router"
-    region = "us-east1"
-    network = "google_compute_network.tf-mod3-lab1-vpc2"
-    depends_on = [google_compute_network.tf-mod3-lab1-vpc2]
+  name       = "tf-mod3-lab1-router"
+  region     = "us-east1"
+  network    = "google_compute_network.tf-mod3-lab1-vpc2"
+  depends_on = [google_compute_network.tf-mod3-lab1-vpc2]
 }
 
 // NAT
 resource "google_computer_router_nat" "nat" {
-    name = "tf-mod3-lab1-nat"
-    router = "tf-mod3-lab1-router"
-    region = google_compute_router.tf-mod3-lab1-router.region
-    nat_ip_allocate_option = "AUTO_ONLY"
-    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-    depends_on = [google_compute_network.tf-mod3-lab1-vpc2,google_compute_router.tf-mod3-lab1-router]
+  name                               = "tf-mod3-lab1-nat"
+  router                             = "tf-mod3-lab1-router"
+  region                             = google_compute_router.tf-mod3-lab1-router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  depends_on                         = [google_compute_network.tf-mod3-lab1-vpc2, google_compute_router.tf-mod3-lab1-router]
 }
 
 // VMs
 resource "google_compute_instance" "tf-mod3-lab1-vm1" {
-  name = "tf-mod3-lab1-vm1"
+  name         = "tf-mod3-lab1-vm1"
   machine_type = "e2-micro"
-  zone = "us-central1-a"  
-  depends_on = [google_compute_network.tf-mod3-lab1-vpc1, google_compute_subnetwork.tf-mod3-lab1-sub1]
+  zone         = "us-central1-a"
+  depends_on   = [google_compute_network.tf-mod3-lab1-vpc1, google_compute_subnetwork.tf-mod3-lab1-sub1]
   network_interface {
     // This indicates to give a public IP address
     // access_config {
     //  network_tier = "STANDARD"
     // }
-    network = "tf-mod3-lab1-vpc1"
+    network    = "tf-mod3-lab1-vpc1"
     subnetwork = "tf-mod3-lab1-sub1"
   }
 
@@ -107,23 +107,23 @@ resource "google_compute_instance" "tf-mod3-lab1-vm1" {
     initialize_params {
       image = "debian-12-bookworm-v20240312"
     }
-  } 
+  }
   metadata = {
     startup-script = "sudo apt update; sudo apt -y install netcat-traditional ncat;"
   }
 }
 
 resource "google_compute_instance" "tf-mod3-lab1-vm2" {
-  name = "tf-mod3-lab1-vm2"
+  name         = "tf-mod3-lab1-vm2"
   machine_type = "e2-micro"
-  zone = "us-east1-b"  
-  depends_on = [google_compute_network.tf-mod3-lab1-vpc2, google_compute_subnetwork.tf-mod3-lab1-sub2]
+  zone         = "us-east1-b"
+  depends_on   = [google_compute_network.tf-mod3-lab1-vpc2, google_compute_subnetwork.tf-mod3-lab1-sub2]
   network_interface {
     // This indicates to give a public IP address
     // access_config {
     //   network_tier = "STANDARD"
     // }
-    network = "tf-mod3-lab1-vpc2"
+    network    = "tf-mod3-lab1-vpc2"
     subnetwork = "tf-mod3-lab1-sub2"
   }
 
@@ -131,7 +131,7 @@ resource "google_compute_instance" "tf-mod3-lab1-vm2" {
     initialize_params {
       image = "debian-12-bookworm-v20240312"
     }
-  } 
+  }
   metadata = {
     startup-script = "sudo apt update; sudo apt -y install netcat-traditional ncat;"
   }
