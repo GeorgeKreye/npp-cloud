0e63640ee80a8c7d4cbedc52f4e8600583e7550f
George Kreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-11-01 23:37:33 -0600

    Lab 3 work finished, add Lab 4 prep

--- tf-mod3-lab1/main.tf
+++ tf-mod3-lab1/main.tf
@@ -11,13 +11,27 @@ terraform {
 resource "google_compute_network" "tf-mod3-lab1-vpc1" {
   name                    = "tf-mod3-lab1-vpc1"
   auto_create_subnetworks = "false"
+  project                 = "kr-m3-l1-cunetworking"
 }
 
 resource "google_compute_network" "tf-mod3-lab1-vpc2" {
   name                    = "tf-mod3-lab1-vpc2"
   auto_create_subnetworks = "false"
+  project                 = "kr-m3-l1-cunetworking"
 }
 
+// VPC Peering
+resource "google_compute_network_peering" "tf-mod3-lab1-peering1" {
+  name         = "tf-mod3-lab1-peering1"
+  network      = google_compute_network.tf-mod3-lab1-vpc1.self_link
+  peer_network = google_compute_network.tf-mod3-lab1-vpc2.self_link
+}
+
+resource "google_compute_network_peering" "tf-mod3-lab1-peering2" {
+  name         = "tf-mod3-lab1-peering2"
+  network      = google_compute_network.tf-mod3-lab1-vpc2.self_link
+  peer_network = google_compute_network.tf-mod3-lab1-vpc1.self_link
+}
 
 // Subnets
 resource "google_compute_subnetwork" "tf-mod3-lab1-sub1" {
@@ -25,6 +39,7 @@ resource "google_compute_subnetwork" "tf-mod3-lab1-sub1" {
   ip_cidr_range = "172.16.0.0/24"
   region        = "us-central1"
   network       = google_compute_network.tf-mod3-lab1-vpc1.id
+  project       = "kr-m3-l1-cunetworking"
 }
 
 resource "google_compute_subnetwork" "tf-mod3-lab1-sub2" {
@@ -32,14 +47,14 @@ resource "google_compute_subnetwork" "tf-mod3-lab1-sub2" {
   ip_cidr_range = "172.16.1.0/24"
   region        = "us-east1"
   network       = google_compute_network.tf-mod3-lab1-vpc2.id
+  project       = "kr-m3-l1-cunetworking"
 }
 
-// Firewall
+// Firewalls
 resource "google_compute_firewall" "tf-mod3-lab1-fwrule1" {
-  project = "kreye-lab1project-cunetworking"
+  project = "kr-m3-l1-cunetworking" 
   name    = "tf-mod3-lab1-fwrule1"
   network = "tf-mod3-lab1-vpc1"
-  // need the network created before the firewall rule
   depends_on = [google_compute_network.tf-mod3-lab1-vpc1]
 
   allow {
@@ -57,10 +72,9 @@ resource "google_compute_firewall" "tf-mod3-lab1-fwrule1" {
 }
 
 resource "google_compute_firewall" "tf-mod3-lab1-fwrule2" {
-  project = "kreye-lab1project-cunetworking"
+  project = "kr-m3-l1-cunetworking" 
   name    = "tf-mod3-lab1-fwrule2"
   network = "tf-mod3-lab1-vpc2"
-  // need the network created before the firewall rule
   depends_on = [google_compute_network.tf-mod3-lab1-vpc2]
 
   allow {
@@ -83,17 +97,38 @@ resource "google_compute_router" "tf-mod3-lab1-router" {
   region     = "us-east1"
   network    = google_compute_network.tf-mod3-lab1-vpc2.id
   depends_on = [google_compute_network.tf-mod3-lab1-vpc2]
+  project    = "kr-m3-l1-cunetworking" 
+}
+
+// Router Interface
+resource "google_compute_router_interface" "tf-mod3-lab1-rtrinterface" {
+  name       = "tf-mod3-lab1-rtrinterface"
+  router     = "tf-mod3-lab1-router"
+  region     = google_compute_router.tf-mod3-lab1-router.region
+  subnetwork = google_compute_subnetwork.tf-mod3-lab1-sub2.id
+  depends_on = [google_compute_router.tf-mod3-lab1-router, google_compute_subnetwork.tf-mod3-lab1-sub2]
+  project    = "kr-m3-l1-cunetworking" 
 }
 
 // NAT
-resource "google_compute_router_nat" "nat" {
+resource "google_compute_router_nat" "tf-mod3-lab1-nat" {
   name                               = "tf-mod3-lab1-nat"
   router                             = "tf-mod3-lab1-router"
   region                             = google_compute_router.tf-mod3-lab1-router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   depends_on                         = [google_compute_network.tf-mod3-lab1-vpc2, google_compute_router.tf-mod3-lab1-router]
-  max_ports_per_vm                   = 1
+  max_ports_per_vm                   = 64
+  project                            = "kr-m3-l1-cunetworking"
+}
+
+// Custom Default Route
+resource "google_compute_route" "tf-mod3-lab1-nat-route" {
+  name             = "tf-mod3-lab1-nat-route"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.tf-mod3-lab1-vpc2.name
+  next_hop_gateway = "default-internet-gateway" 
+  project          = "kr-m3-l1-cunetworking" 
 }
 
 // VMs
@@ -101,14 +136,12 @@ resource "google_compute_instance" "tf-mod3-lab1-vm1" {
   name         = "tf-mod3-lab1-vm1"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
+  project      = "kr-m3-l1-cunetworking" 
   depends_on   = [google_compute_network.tf-mod3-lab1-vpc1, google_compute_subnetwork.tf-mod3-lab1-sub1]
   network_interface {
-    // This indicates to give a public IP address
-    // access_config {
-    //  network_tier = "STANDARD"
-    // }
     network    = "tf-mod3-lab1-vpc1"
     subnetwork = "tf-mod3-lab1-sub1"
+    subnetwork_project = "kr-m3-l1-cunetworking"
   }
 
   boot_disk {
@@ -125,14 +158,12 @@ resource "google_compute_instance" "tf-mod3-lab1-vm2" {
   name         = "tf-mod3-lab1-vm2"
   machine_type = "e2-micro"
   zone         = "us-east1-b"
+  project      = "kr-m3-l1-cunetworking" 
   depends_on   = [google_compute_network.tf-mod3-lab1-vpc2, google_compute_subnetwork.tf-mod3-lab1-sub2]
   network_interface {
-    // This indicates to give a public IP address
-    // access_config {
-    //   network_tier = "STANDARD"
-    // }
     network    = "tf-mod3-lab1-vpc2"
     subnetwork = "tf-mod3-lab1-sub2"
+    subnetwork_project = "kr-m3-l1-cunetworking"
   }
 
   boot_disk {
